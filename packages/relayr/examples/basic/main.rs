1c95fc502a015eb8c4012462dc43a854370bc202
Dmndz <barbara@dsplce.co>
2025-05-01 23:05:45 +0200

    feat: introduce error callbacks
    Closes #1

--- packages/relayr/examples/basic/main.rs
+++ packages/relayr/examples/basic/main.rs
@@ -1,11 +1,24 @@
+use std::time::Duration;
+use chrono::Local;
+use tokio::time::sleep;
 use relayr::prelude::*;
 
-#[relayr::cron("1/1 * * * * *")]
-async fn print_message_every_second(_: JobId) {
+#[cron("1/1 * * * * *")]
+async fn print_message_every_second(_: JobId) -> anyhow::Result<()> {
     println!("🖤 Hello from relayr 0.3.0!");
+
+    sleep(Duration::from_secs(3)).await;
+    Err(anyhow::anyhow!("Something went wrong"))?;
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    relayr::run().await
+    relayr::set_error_callback(|job_id: JobId, job_name: &'static str, error: anyhow::Error| async move {
+        println!("Error in job {:?} ({:?}):", job_name, job_id);
+        println!("> {:?}\n", error);
+    }).await;
+
+    relayr::run::<Local>().await
 }
--- packages/relayr/src/cron.rs
+++ packages/relayr/src/cron.rs
@@ -1,10 +1,12 @@
 use std::pin::Pin;
 use async_cron_scheduler::JobId;
+use anyhow::Result;
 
 #[derive(Debug)]
 pub struct Cron {
     pub pattern: &'static str,
-    pub runnable: fn(JobId) -> Pin<Box<dyn Future<Output = ()> + Send>>,
+    pub runnable: fn(JobId) -> Pin<Box<dyn Future<Output = Result<()>> + Send>>,
+    pub name: &'static str,
 }
 
 inventory::collect!(Cron);
