542efd981dc1e92de664afd01e4ec1f62c5e64ba
xkcm <xkcm16@gmail.com>
2026-03-04 16:34:56 +0100

    feat: add env variable support

--- packages/relayr/src/cron.rs
+++ packages/relayr/src/cron.rs
@@ -4,9 +4,24 @@ use anyhow::Result;
 
 #[derive(Debug)]
 pub struct Cron {
-    pub pattern: &'static str,
+    pub pattern: CronPattern,
     pub runnable: fn(JobId) -> Pin<Box<dyn Future<Output = Result<()>> + Send>>,
     pub name: &'static str,
+}
+
+#[derive(Debug)]
+pub enum CronPattern {
+    Literal(&'static str),
+    EnvironmentVariable(&'static str)
+}
+
+impl CronPattern {
+    pub fn resolve(&self) -> Option<String> {
+        match self {
+            Self::Literal(literal_pattern) => Some(literal_pattern.to_string()),
+            Self::EnvironmentVariable(variable_name) => std::env::var(variable_name).ok()
+        }
+    }
 }
 
 inventory::collect!(Cron);
